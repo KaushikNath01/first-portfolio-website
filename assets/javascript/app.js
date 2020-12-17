b34a9016cdc617764bf27f2fd0ef43d713bdafb6
Kaushik Nath <kaush.kn.735@gmail.com>
2020-12-17 22:59:26 +0530

    added a favicon

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,78 +18,35 @@ function toggleMenu () {
    }
 }
 
-
+const form = document.querySelector('.form')
 const fullName = document.querySelector('.fullName')
 const mobileNo = document.querySelector('.mobileNo')
 const email = document.querySelector('.email')
 const password = document.querySelector('.password')
 const confirmPassword = document.querySelector('.confirmPassword')
-const formBtn = document.querySelector('.form-signIn-btn');
 
-
-formBtn.addEventListener('click',(e) => {
-//    e.preventDefault();
-
-   const nameId = document.getElementById('fullNameId');
-   const mobileId = document.getElementById('mobileId');
-   const emailId = document.getElementById('emailId');
-   const passwordId = document.getElementById('passwordId');
-   let formError = false;
-   
-   if ((fullName.value === "") || (fullName.value === null)) {
-      nameId.innerHTML = 'Please add something in the name field'
-      formError = true;
-  }else if ((fullName.value.length < 5)||(fullName.value.length > 20)) {
-       nameId.innerHTML = 'the length should not be less than 5 and not more than 20';
-       formError = true;
-  }else{
-       nameId.innerHTML = '';
-       formError = false;
-  }
-
-  if ((mobileNo.value === "") || (mobileNo.value === null)) {
-      mobileId.innerHTML = 'Please add something in the name field'
-      formError = true;
-  }else if ((mobileNo.value.length < 10)||(mobileNo.value.length > 11)) {
-       mobileId.innerHTML = 'mobile no should not be less than 10 and exceed 10 nos';
-       formError = true;
-  }else {
-       mobileId.innerHTML = '';
-       formError = false;
-  }
-
-  if ((email.value === "") || (email.value === null)) {
-      emailId.innerHTML = 'Please add something in the name field'
-      formError = true;
-  }else {
-     emailId.innerHTML = '';
-     formError = false;
-  }
-
-  if ((password.value === "") || (password.value === null)) {
-       passwordId.innerHTML = 'Please add something in the name field'
-       formError = true;
-  }else if ((password.value.length < 8)||(password.value.length > 16)) {
-       passwordId.innerHTML = 'Password should not be less than 8 and exceed 16';
-       formError = true;
-  }else{
-       passwordId.innerHTML = '';
-       formError = false;
-  }
-
-   if (formError = true){
-      return false;
-   }
-      return true;
-  
+form.addEventListener('submit',(e) => {
+   e.preventDefault();
+      validateForm()
 });
+
+const validateForm = () => {
+     const fullName = fullName.value.trim();
+     const mobileNo = mobileNo.value.trim();
+     const email = email.value.trim();
+     const password = password.value.trim();
+     const confirmPassword = confirmPassword.value.trim();
+
+     if (fullName === "") {
+          setErrormsg()
+     }
+}
 
 
 const instaNo = document.querySelector('.instaNos');
 const fundsGenerated = document.querySelector('.fundsGenerated');
 const RevenueGenerated = document.querySelector('.RevenueGenerated');
 const SatisfiedCustomers = document.querySelector('.SatisfiedCustomers');
-
 
 let count = 1;
 setInterval(() => {
